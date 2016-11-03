7d41138b0e0e078a33fd56c6eaec294a32b556a4
Jared <jaredlgw@outlook.com>
2016-11-03 16:20:26 +1100

    Started SQL testing

---
+++ src/sqltest/SQLiteJDBCtest.java
@@ -0,0 +1,19 @@
+package sqltest;
+
+import java.sql.*;
+
+@SuppressWarnings("unused")
+public class SQLiteJDBCtest{
+	public static void main( String args[] )
+	{
+		Connection c = null;
+		try {
+			Class.forName("org.sqlite.JDBC");
+			c = DriverManager.getConnection("jdbc:sqlite:test.db");
+		} catch ( Exception e ) {
+			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+			System.exit(0);
+		}
+		System.out.println("Opened database successfully");
+	}
+}
